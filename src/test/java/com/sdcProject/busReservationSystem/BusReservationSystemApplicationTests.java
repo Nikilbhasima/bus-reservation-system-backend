417941159d920693df9171308d98a06ee9115e90
NIKIBHASIMA <nikilbhasima534@gmail.com>
2025-08-28 20:11:28 +0545

    first commit

---
+++ src/test/java/com/sdcProject/busReservationSystem/BusReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdcProject.busReservationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
